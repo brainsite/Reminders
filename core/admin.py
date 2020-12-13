f33a12b95613e792b6cbd3a392385feb71d5f968
brainsite <site@brainsite.ru>
2020-12-13 23:03:50 +0300

    first commit

--- core/admin.py
+++ core/admin.py
@@ -7,8 +7,8 @@ from .models import Notification, RemindU
 class AdminNotification(admin.ModelAdmin):
     list_display = ('user_name', 'text_notification', 'datetime_notification', 'completed')
 
-@admin.register(RemindU)
-class RemindUAdmin(admin.ModelAdmin):
+
+class RemindUAdmin(RemindU):
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'lang')}),
@@ -20,3 +20,4 @@ class RemindUAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'telegram','lang', 'first_name', 'last_name', 'is_staff')
 
 # Register your models here.
+admin.site.register(RemindU, RemindUAdmin)

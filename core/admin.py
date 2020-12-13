5ad06f8b4825e91e71a8adc3045aaff185a06359
brainsite <site@brainsite.ru>
2020-12-13 23:20:00 +0300

    first commit

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from .models import Notification, RemindU
 
@@ -8,16 +10,17 @@ class AdminNotification(admin.ModelAdmin):
     list_display = ('user_name', 'text_notification', 'datetime_notification', 'completed')
 
 
-class RemindUAdmin(RemindU):
+class RemindUAdmin(UserAdmin):
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'lang')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'telegram')}),
         (_('Permissions'), {
             'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
         }),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
-    list_display = ('username', 'email', 'telegram','lang', 'first_name', 'last_name', 'is_staff')
+    list_display = ('username', 'email', 'telegram', 'first_name', 'last_name', 'is_staff')
+
 
 # Register your models here.
 admin.site.register(RemindU, RemindUAdmin)
